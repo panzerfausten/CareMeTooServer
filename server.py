50dbd07f96316473bc12f9e3627107e8ad4f5871
Darien Miranda <panzerfausten@gmail.com>
2014-11-16 00:00:58 -0700

    file upload system

--- server.py
+++ server.py
@@ -5,6 +5,17 @@ app = Flask(__name__)
 from ViewRenderer import ViewRenderer
 from models import CareGiver
 from models import Data
+
+######################upload files
+import os
+from flask import redirect, url_for
+from werkzeug import secure_filename
+
+UPLOAD_FOLDER = 'files/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+###################
 @app.route("/")
 def hello():
 	    return "Hello World!"
@@ -47,5 +58,32 @@ def careGiverData(caregiver_id):
 		return _res.toJson()
 	else:
 		return "Method not supported"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/files', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "file uploaded modafaka"#redirect(url_for('uploaded_file',
+                    #                filename=filename))
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form action="" method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
+
+
 if __name__ == "__main__":
+	app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 	app.run(debug=True,host='0.0.0.0')
